4e0b0ead0e74bdca033ae055616d940636c4398d
KhalidOmer <khalid.hassan@aims.ac.rw>
2017-05-19 14:07:58 +0200

    gallery

--- uncertainity_in_pi_calculation.py
+++ uncertainity_in_pi_calculation.py
@@ -1,24 +1,38 @@
-from Calculation_of_pi import *
-# the uncertainity in the calculation
 
-def pi_gen():
-	l =[]
-	for i in range(1000):
-		l.append(pi())
-	return l
-l3 = []
+# the uncertainity in the calculation
+import numpy as np
+import matplotlib.pylab as plt
+def pi(n):
+    a = 0
+    for i in range(n):
+        x = np.random.uniform(-1,1)
+        y = np.random.uniform(-1,1)
+        if x**2 +y**2 <=1:
+            a += 1
+    return (a/n)*4
+n = []    
+for i in range(10,10010,100):
+	n.append(i) 
+print(n)	
+	   
 sigma = []
-ji = []
-for j in range(1000):
-	print(j)
-	for i in range(j):
-		l3.append(pi())
+for i in n:
+	print(i)
+	l3 = []
+	for j in range(1000):
+		l3.append(pi(i))
 	sig = np.std(l3)
-	ji.append(j)
 	sigma.append(sig)
-plt.plot(ji,sigma, 'blue')
+#plt.plot(n,sigma, 'blue')
+#plt.ylabel("The error in the calculation")
+#plt.xlabel("Number of samples used to calculate pi")
+#plt.title('The uncertainity in pi calculation')
+#plt.show()
+
+plt.semilogx(n, sigma, 'blue')
 plt.ylabel("The error in the calculation")
 plt.xlabel("Number of samples used to calculate pi")
 plt.title('The uncertainity in pi calculation')
 plt.show()
+
 
